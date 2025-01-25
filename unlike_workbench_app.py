bb442c06832b381ec11a38c6fbfc1ada637f5fce
David Adams <80413862+davidkadams@users.noreply.github.com>
2025-01-24 20:20:42 -0500

    Update unlike_workbench_app.py

--- unlike_workbench_app.py
+++ unlike_workbench_app.py
@@ -25,7 +25,7 @@ def check_password():
 
     # Show input for password.
     st.text_input(
-        "Password", type="password", on_change=password_entered, key="password"
+        "PasswordLOL", type="password", on_change=password_entered, key="password"
     )
     if "password_correct" in st.session_state:
         st.error("😕 Password incorrect")
